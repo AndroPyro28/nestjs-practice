cc1fa1825b756ef8578fce89968e6678f8a6980f
Menandro Eugenio <menandroeugenio1028@gmail.com>
2022-09-10 23:24:35 +0800

    socket

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CustomersModule } from './routes/customers/customers.module';
-import { AdminModule } from './routes/admin/admin.module';
+import { CustomersModule } from './customers/customers.module';
+import { AdminModule } from './admin/admin.module';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaUserModule } from './routes/prisma-user/prisma-user.module';
-import { AuthModule } from './routes/auth/auth.module';
-import { PaymentModule } from './routes/payment/payment.module';
-import { GatewayModule } from './routes/websocket/gateway.module';
+import { PrismaUserModule } from './prisma-user/prisma-user.module';
+import { AuthModule } from './auth/auth.module';
+import { PaymentModule } from './payment/payment.module';
+import { GatewayModule } from './websocket/gateway.module';
 
 @Module({
   imports: [
