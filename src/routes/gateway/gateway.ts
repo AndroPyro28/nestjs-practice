7376f7b18c2d6a82342e558bb05ee28bcca36e9d
Menandro Eugenio <menandroeugenio1028@gmail.com>
2022-09-10 23:17:29 +0800

    socket

--- src/routes/gateway/gateway.ts
+++ src/routes/gateway/gateway.ts
@@ -8,10 +8,10 @@ import { Server } from "socket.io";
 })
 export class MyGateway implements OnModuleInit {
 
-    @WebSocketServer()
+    @WebSocketServer() // this is decorator that tells that this application is a server of a socket
     private readonly io: Server;
 
-    onModuleInit() {
+    onModuleInit() { // a on module init method is a method from OnModuleInit interface so we need to follow it and it is the only method that we can only put a io server
         this.io.on('connection', (socket) => {
             socket.on('newMessage', (body) => {
                 console.log(body)
@@ -19,4 +19,6 @@ export class MyGateway implements OnModuleInit {
             })
         })
     }
+
+    
 } 
